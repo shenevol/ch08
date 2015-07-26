8ddeea2320e62c0a750cce8b33752c83d3a77496
Jason Huang <cjhuang@yahoo-inc.com>
2015-07-26 22:02:04 +0800

    save data to server

--- begin/app.js
+++ begin/app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
+
 var db = {
   'finishers': [
     {
@@ -63,23 +67,29 @@ var db = {
 };
 
 app.get('/', function(req, res) {
-	//req is used to accept data from client
-	//res is used to pass data to client
-	res.header('Access-Control-Allow-Origin', '*');
-	res.send(new Date());
+    //req is used to accept data from client
+    //res is used to pass data to client
+    res.header('Access-Control-Allow-Origin', '*');
+    res.send(new Date());
 });
 
 app.get('/finishers.json', function(req, res) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.json(db);
+    res.header('Access-Control-Allow-Origin', '*');
+    res.json(db);
 });
 
 app.post('/service', function(req, res) {
-	res.header('Access-Control-Allow-Origin', '*');
-	console.log('post ok');
-	console.log(runners);
-	runners.push('foo');
-	res.sendStatus(200);
+    res.header('Access-Control-Allow-Origin', '*');
+
+    var finisherData = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        gender: req.body.gender,
+        time: req.body.finMin + ':' + req.body.finSec
+    };
+    db.finishers.push(finisherData);
+
+    res.sendStatus(200);
 });
 
 console.log('node server listen on port: 3000');
